b3ac53b2e217fe69b4ccbe56e7f6c637c18d7c6c
Neo <liveabean@163.com>
2020-07-30 20:00:03 +0800

    feat: 237. 删除链表中的节点

---
+++ 237.删除链表中的节点.js
@@ -0,0 +1,79 @@
+/*
+ * @lc app=leetcode.cn id=237 lang=javascript
+ *
+ * [237] 删除链表中的节点
+ *
+ * https://leetcode-cn.com/problems/delete-node-in-a-linked-list/description/
+ *
+ * algorithms
+ * Easy (82.27%)
+ * Likes:    725
+ * Dislikes: 0
+ * Total Accepted:    114.4K
+ * Total Submissions: 139K
+ * Testcase Example:  '[4,5,1,9]\n5'
+ *
+ * 请编写一个函数，使其可以删除某个链表中给定的（非末尾）节点，你将只被给定要求被删除的节点。
+ * 
+ * 现有一个链表 -- head = [4,5,1,9]，它可以表示为:
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 示例 1:
+ * 
+ * 输入: head = [4,5,1,9], node = 5
+ * 输出: [4,1,9]
+ * 解释: 给定你链表中值为 5 的第二个节点，那么在调用了你的函数之后，该链表应变为 4 -> 1 -> 9.
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 输入: head = [4,5,1,9], node = 1
+ * 输出: [4,5,9]
+ * 解释: 给定你链表中值为 1 的第三个节点，那么在调用了你的函数之后，该链表应变为 4 -> 5 -> 9.
+ * 
+ * 
+ * 
+ * 
+ * 说明:
+ * 
+ * 
+ * 链表至少包含两个节点。
+ * 链表中所有节点的值都是唯一的。
+ * 给定的节点为非末尾节点并且一定是链表中的一个有效节点。
+ * 不要从你的函数中返回任何结果。
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} node
+ * @return {void} Do not return anything, modify node in-place instead.
+ */
+/**
+ * Demo:
+ * 4->5->1->9
+ * 4->1->1->9
+ * 4->5->9  1->9
+ * 4->5->9  1->null
+ */
+var deleteNode = function(node) {
+    // if (node == null) { return; }
+    // if (node.next == null) { delete node.val; return; }
+    node.val = node.next.val;
+    const next = node.next;
+    node.next = node.next.next;
+    next.next = null;
+}
+// @lc code=end
+
