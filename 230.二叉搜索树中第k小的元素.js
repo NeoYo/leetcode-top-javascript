75feca9ff46e61a71a3b7c48fe849619c64937ea
Neo <liveabean@163.com>
2020-08-07 23:58:23 +0800

    feat: 二叉搜索树种第k小的元素  Resolution: 中序遍历

---
+++ 230.二叉搜索树中第k小的元素.js
@@ -0,0 +1,82 @@
+/*
+ * @lc app=leetcode.cn id=230 lang=javascript
+ *
+ * [230] 二叉搜索树中第K小的元素
+ *
+ * https://leetcode-cn.com/problems/kth-smallest-element-in-a-bst/description/
+ *
+ * algorithms
+ * Medium (71.29%)
+ * Likes:    254
+ * Dislikes: 0
+ * Total Accepted:    63.4K
+ * Total Submissions: 88.8K
+ * Testcase Example:  '[3,1,4,null,2]\n1'
+ *
+ * 给定一个二叉搜索树，编写一个函数 kthSmallest 来查找其中第 k 个最小的元素。
+ * 
+ * 说明：
+ * 你可以假设 k 总是有效的，1 ≤ k ≤ 二叉搜索树元素个数。
+ * 
+ * 示例 1:
+ * 
+ * 输入: root = [3,1,4,null,2], k = 1
+ * ⁠  3
+ * ⁠ / \
+ * ⁠1   4
+ * ⁠     \
+ *       2
+ * 输出: 1
+ * 
+ * 示例 2:
+ * 
+ * 输入: root = [5,3,6,2,4,null,null,1], k = 3
+ * ⁠      5
+ * ⁠     / \
+ * ⁠    3   6
+ * ⁠   / \
+ * ⁠  2   4
+ * ⁠ /
+ * ⁠1
+ * 输出: 3
+ * 
+ * 进阶：
+ * 如果二叉搜索树经常被修改（插入/删除操作）并且你需要频繁地查找第 k 小的值，你将如何优化 kthSmallest 函数？
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} k
+ * @return {number}
+ */
+var kthSmallest = function(root, k) {
+    let cursor = 1;
+    const inorder = function(root) {
+        if (root == null) { return; }            
+        const { left, right } = root;
+        inorder(left);
+        if (cursor === k) {
+            // 注意：这里 return 不能终止所有递归
+            throw root.val;
+        }
+        cursor++;
+        inorder(right);
+    }
+    try {
+        inorder(root);
+    } catch(val) {
+        return val;
+    }
+};
+// @lc code=end
+
