b14b710bd6d529d28fb3f1f50d8536d3af8ab96d
Neo <liveabean@163.com>
2020-10-13 21:32:06 +0800

    feat: 887. 鸡蛋掉落递归思路和代码

---
+++ 887.鸡蛋掉落.js
@@ -0,0 +1,121 @@
+/*
+ * @lc app=leetcode.cn id=887 lang=javascript
+ *
+ * [887] 鸡蛋掉落
+ *
+ * https://leetcode-cn.com/problems/super-egg-drop/description/
+ *
+ * algorithms
+ * Hard (28.67%)
+ * Likes:    497
+ * Dislikes: 0
+ * Total Accepted:    32.1K
+ * Total Submissions: 111.9K
+ * Testcase Example:  '1\n2'
+ *
+ * 你将获得 K 个鸡蛋，并可以使用一栋从 1 到 N  共有 N 层楼的建筑。
+ * 
+ * 每个蛋的功能都是一样的，如果一个蛋碎了，你就不能再把它掉下去。
+ * 
+ * 你知道存在楼层 F ，满足 0 <= F <= N 任何从高于 F 的楼层落下的鸡蛋都会碎，从 F 楼层或比它低的楼层落下的鸡蛋都不会破。
+ * 
+ * 每次移动，你可以取一个鸡蛋（如果你有完整的鸡蛋）并把它从任一楼层 X 扔下（满足 1 <= X <= N）。
+ * 
+ * 你的目标是确切地知道 F 的值是多少。
+ * 
+ * 无论 F 的初始值如何，你确定 F 的值的最小移动次数是多少？
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 输入：K = 1, N = 2
+ * 输出：2
+ * 解释：
+ * 鸡蛋从 1 楼掉落。如果它碎了，我们肯定知道 F = 0 。
+ * 否则，鸡蛋从 2 楼掉落。如果它碎了，我们肯定知道 F = 1 。
+ * 如果它没碎，那么我们肯定知道 F = 2 。
+ * 因此，在最坏的情况下我们需要移动 2 次以确定 F 是多少。
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 输入：K = 2, N = 6
+ * 输出：3
+ * 
+ * 
+ * 示例 3：
+ * 
+ * 输入：K = 3, N = 14
+ * 输出：4
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 1 <= K <= 100
+ * 1 <= N <= 10000
+ * 
+ * 
+ */
+/**
+    题解:
+
+    参考资料: 
+        liweiwei1419 动态规划 https://leetcode-cn.com/problems/super-egg-drop/solution/dong-tai-gui-hua-zhi-jie-shi-guan-fang-ti-jie-fang/
+        这篇文章讲了、递归 -> 递归+备忘录 -> 动态规划 -> 动态规划+二分查找，讲得很好！
+
+    动态规划（递归）初始值
+        1. eggs K=0 或 floors N=0, 结果为0，因为没有鸡蛋可以扔，或者有鸡蛋没有楼层扔，只能等于 0
+        2. floors N=1, eggs K >= 1, 结果为 1，因为只能扔一次，只能在floor 1 楼扔 1 <= X <= N = 1
+        3. eggs K=1, floors N >= 1, 结果为 N，因为只能从下往上，因为只有一个蛋
+
+    递归公式（递推公式）
+                    eggs floors
+        superEggDrop(K,    N)
+        
+        K, N 的条件下，在 X 层扔，会出现的情形
+            1. 鸡蛋不碎: superEggDrop(K, N-X)    // N-X 范围是 (X, N]
+            2. 鸡蛋碎了: superEggDrop(K-1, X-1)  // X 已经扔过，所以是 [0, X-1]
+
+        不受控制的碎与不碎
+            真实情况，可能碎，也可能不碎，不受我们控制，但是真实情况只可能两种中的一种
+            取两种中的最大值，可以保证计算得到的步数，一定能得到楼层 F
+            Math.max(superEggDrop(K-1, X-1), superEggDrop(K, N-X))
+
+        可以做的选择，在哪一个楼层(X)扔
+            superEggDrop(K-1, X-1), superEggDrop(K, N-X) 中 X 范围是 [0, N]，相当于有 2N个子树
+            虽然不知道它们等于多少，但是可以当它们是一个个已知的整体，交给 递归 去解决~~~
+
+            接下来，选择那个需要的步数最少的，可以得到最优解，可以得到递归公式
+
+        递归公式
+            for (let i = 1; X <= N; X++) {
+                superEggDrop(K, N) = Math.min(superEggDrop(K, N), Math.max(superEggDrop(K-1, X-1), superEggDrop(K, N-X)))
+            }
+            
+        
+ */
+// @lc code=start
+/**
+ * @param {number} K eggs
+ * @param {number} N floors
+ * @return {number}
+ */
+var superEggDrop = function(K, N) {
+    if (K === 0 || N === 0) { return 0; }
+    if (K === 1) { return N; }
+    if (N === 1) { return 1; }
+    let nextStep = Infinity;
+    for (let X = 1; X <= N; X++) {
+        nextStep = Math.min(nextStep, Math.max(superEggDrop(K-1, X-1), superEggDrop(K, N-X)) + 1)
+    }
+    return nextStep;
+};
+// @lc code=end
+
