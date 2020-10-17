8399bd1200a2406d35210b9d2389b2d69640a254
Neo <liveabean@163.com>
2020-10-17 23:42:24 +0800

    doc: update README.md

--- 887.鸡蛋掉落.js
+++ 887.鸡蛋掉落.js
@@ -149,6 +149,25 @@
             }
         }
         return l;
+
+        我的二分查找模板代码：
+
+        const bsearch = (nums, target) => {
+            let left = 0;
+            let right = nums.length - 1;
+
+            while (left <= right) {
+                let mid = left + (right - left) / 2;
+                if (nums[mid] == target) {
+                return mid;
+                } else if (nums[mid] < target) {
+                left = mid + 1;
+                } else {
+                right = mid - 1;
+                }
+            }
+            return -1;
+        }
                 
  */
 /**
@@ -235,7 +254,7 @@ var superEggDrop = function(K, N) {
             const notBreak = (x) => (DP[k][n-x]);   // \
             let l = 1,
                 r = n;
-            while (l < r) {
+            while (l <= r) {
                 const mid = l + ((r - l)>>1);
                 if (eggBreak(mid) === notBreak(mid)) {
                     l = mid;
