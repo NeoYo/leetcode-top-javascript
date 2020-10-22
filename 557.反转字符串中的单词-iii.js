52ff27b766a325e3faae315ca1a94b18dc361a1c
Neo <liveabean@163.com>
2020-10-22 21:26:30 +0800

    feat: 557. 反转字符中的单词-iii

---
+++ 557.反转字符串中的单词-iii.js
@@ -0,0 +1,55 @@
+/*
+ * @lc app=leetcode.cn id=557 lang=javascript
+ *
+ * [557] 反转字符串中的单词 III
+ *
+ * https://leetcode-cn.com/problems/reverse-words-in-a-string-iii/description/
+ *
+ * algorithms
+ * Easy (73.41%)
+ * Likes:    248
+ * Dislikes: 0
+ * Total Accepted:    99.5K
+ * Total Submissions: 135.4K
+ * Testcase Example:  `"Let's take LeetCode contest"`
+ *
+ * 给定一个字符串，你需要反转字符串中每个单词的字符顺序，同时仍保留空格和单词的初始顺序。
+ * 
+ * 
+ * 
+ * 示例：
+ * 
+ * 输入："Let's take LeetCode contest"
+ * 输出："s'teL ekat edoCteeL tsetnoc"
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 在字符串中，每个单词由单个空格分隔，并且字符串中不会有任何额外的空格。
+ * 
+ * 
+ */
+/**
+    知识点
+        str.split(' ') = str.split('/\s/g')
+        str.match(/[\w']+/g)  
+ */
+// @lc code=start
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseWords = function(s) {
+    return (
+       s.split(' ')
+        .map((wordStr) => (
+            wordStr.split('').reverse().join('')
+        ))
+        .join(' ')
+    );
+};
+// @lc code=end
+
