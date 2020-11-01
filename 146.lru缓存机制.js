2f96fa415eae3ca1b82b4da30b25f000b62b3a19
Neo <liveabean@163.com>
2020-11-01 21:51:46 +0800

    fix: make codes to work.

--- 146.lru缓存机制.js
+++ 146.lru缓存机制.js
@@ -43,6 +43,9 @@
  * cache.get(4);       // 返回  4
  */
 /*
+参考资料
+    https://leetcode-cn.com/problems/lru-cache/solution/bu-yong-yu-yan-nei-jian-de-map-gua-dang-feng-zhuan/
+
 零、笔记
     LRU Least Recently Used   最近最少使用
         解释
@@ -52,28 +55,28 @@
 
 一、LRUCache.prototype.get => 哈希容器
     题目要求 LRUCache.prototype.get 时间复杂度是 O(1)， 那么哈希容器就是这样的数据结构
-    由于考虑到移动端的兼容性，低端机型需要对 ES6 Map 做 polyfill 处理，会影响到 js 体积大小，从而影响到 js 加载速度
-    （戏怎么这么多hhh~~~）
+    由于考虑到移动端的兼容性，低端机型需要对 ES6 Map 做 polyfill 处理，会影响到 js 体积大小，从而影响到 js 加载速度    
     这里直接使用对象 模拟 ES6 Map，有没有更好的数据结构呢？
-    我们看下这里 LRUCache.prototype.put 的 @param {number} key 是 number，那我们可以直接使用数组作为 hash 容器
+    我们看下这里 LRUCache.prototype.put 的 @param {number} key 是 number，这里我们可以直接使用数组作为 hash 容器
+    但是数组不确定 V8 内核，是不是连续空间，是的话，内存碎片会比较多。
+    （戏怎么这么多hhh~~~）
+    综合考虑，这里我们使用 Object
 
 二、LRUCache.prototype.put => 双向链表
     题目要求 LRUCache.prototype.put 的第一个参数 key 是任意的，时间复杂是 O(1)
-    分析可知，put 实际上有两个功能，在 capacity 不够时删掉“任意”节点的元素再添加，在 capacity 够时添加元素
-    满足删除任意节点，时间复杂度是 O(1) 的数据结构是 双向链表（单向链表只做到了在任意元素新增节点是O(1)）
+    分析可知，put 实际上有两个功能，在 space 不够时删掉“任意”节点的元素再添加，在 space 够时添加元素，同时移到头部
+    满足删除任意节点，时间复杂度是 O(1) 的数据结构是 双向链表（单向链表只做到了在任意已知元素新增节点是O(1)）
 
 三、LRU
     LRU 意思是 最近最少使用的先删掉
     调用 put 时，会有以下步骤
         1. 判断新插入元素的 key 是否在 Map 已存在，存在就从链表里删掉 (Map 不用管） —— 删
-        2. 将新元素放在链表的尾部（表示最近一次刚使用），同时存进 Map —— 存
-        3. 检查当前元素总数是否大于 capacity，如果大于，将链表的头元素删掉 （头元素表示最近最少使用的元素）—— 净化
+        2. 将新元素放在链表的头部（表示最近一次刚使用），同时存进 Map —— 存
+        3. 检查 space，将链表的尾元素删掉 （尾元素表示最近最少使用的元素）—— 净化
 
     调用 get 时，会有以下步骤
         1. 判断 get 的 key 是否在 Map 已存在，存在就从链表里删掉 (Map 不用管）—— 删
-        2. 将 key 放在链表的尾部（表示最近一次刚使用）(Map 不用管）—— 存
-        3. (不需要： 检查当前元素总数是否大于 capacity，如果大于，将链表的头元素删掉 （头元素表示最近最少使用的元素）)
-            (因为 get 不会新增，先删后存，是更新)
+        2. 将 key 放在链表的头部（表示最近一次刚使用）(Map 不用管）—— 存
 
 四、准备数据结构
     实现双向链表
@@ -81,127 +84,87 @@
         Em... TypeScirpt 在 LeetCode 上跑不通，那么我们就与国际接轨，参考 [狗头.jpg] github 上的开源库 https://github.com/trekhleb/javascript-algorithms/tree/master/src/data-structures/doubly-linked-list
  */
 // @lc code=start
-class DoublyLinkedListNode {
-    constructor(value, next = null, previous = null) {
-        this.value = value;
-        this.next = next;
-        this.previous = previous;
-    }
-}
-class DoublyLinkedList {
-    constructor() {
-        this.head = null;
-        this.tail = null;
-    }
-    /**
-     * @param {*} value
-     * @return {DoublyLinkedList}
-     */
-    append(value) {
-      const newNode = new DoublyLinkedListNode(value);
-
-      if (!this.head) {
-        this.head = newNode;
-        this.tail = newNode;
-
-        return this;
-      }
-
-      this.tail.next = newNode;
-      newNode.previous = this.tail;
-      this.tail = newNode;
-
-      return this;
-    }
-    deleteTail() {
-    }
-    deleteHead() {
-        if (!this.head) {
-            return null;
-        }
-
-        const deletedHead = this.head;
-
-        if (this.head.next) {
-            this.head = this.head.next;
-            this.head.previous = null;
-        } else {
-            this.head = null;
-            this.tail = null;
-        }
-        return deletedHead;
-    }
-}
-/**
- * @param {number} capacity
- */
-var LRUCache = function(capacity) {
-    this.map = [];                            // Map:  <index, node>
-    this.linkedList = new DoublyLinkedList(); // DoublyLinkedListNode: <{value, key}>
-            // 这里的 value 是 LRU 的 value，不是 linkedList 的 append(value)。{value,key} 组成了 linkedlist 的 value
-    this.size = 0;
-    this.capacity = capacity;
-};
-
-/** 
- * @param {number} key
- * @return {number}
- */
-LRUCache.prototype.get = function(key) {
-    if (this.map[key] == null) {
-        return -1;
-    }
-    // 已存在
-    // 1. 删 - 判断 get 的 key 是否在 Map 已存在，存在就从链表里删掉 (Map 不用管）
-    const node = this.map[key];
-    const nodeValue = node.value; // {value:LRU value, key: LRU key}
-    if (node.previous) {
-        node.previous.next = node.next;
-    } else {
-        // 注意：没有 previous 表示 get 的是头结点
-        return nodeValue.value;
-    }
-    // 2. 存 - 将 key 放在链表的尾部（表示最近一次刚使用）(Map 不用管）
-    this.linkedList.append({ value: nodeValue.value, key });
-    return nodeValue.value;
-};
-
-/** 
- * @param {number} key 
- * @param {number} value
- * @return {void}
- */
-LRUCache.prototype.put = function(key, value) {
-    // 1. 删 - 判断新插入元素的 key 是否在 Map 已存在，存在就从链表里删掉 (Map 不用管）
-    if (this.map[key] != null) {                  // 已存在
-        let node = this.map[key];
-        if (node.previous) {
-            node.previous.next = node.next;
-        } else {
-            // 注意：没有 previous 表示 get 的是头结点
-        }
-        node = null;
-    }
-    // 2. 存 - 将新元素放在链表的尾部（表示最近一次刚使用），同时存进 Map
-    const newNodeValue = { key, value };
-    this.linkedList.append(newNodeValue);
-    this.map[key] = this.linkedList.tail;
-    this.size++;
-    // 3. 净化 - 检查当前元素总数是否大于 capacity，如果大于，将链表的头元素删掉 （头元素表示最近最少使用的元素）
-    if (this.size > this.capacity) {
-        const { key } = this.linkedList.head.value; // {value:LRU value, key: LRU key}
-        this.map[key] = null;
-        this.linkedList.deleteHead();
-        this.size--;
-    }
-};
-
 /**
  * Your LRUCache object will be instantiated and called as such:
  * var obj = new LRUCache(capacity)
  * var param_1 = obj.get(key)
  * obj.put(key,value)
  */
+class ListNode {
+    constructor(key, value) {
+        this.key = key
+        this.value = value
+        this.next = null
+        this.prev = null
+    }
+}
+
+class LRUCache {
+    constructor(capacity) {
+        this.capacity = capacity
+        this.hashTable = {}
+        this.count = 0
+        this.dummyHead = new ListNode()
+        this.dummyTail = new ListNode()
+        this.dummyHead.next = this.dummyTail
+        this.dummyTail.prev = this.dummyHead
+    }
+
+    get(key) {
+        let node = this.hashTable[key]
+        if (node == null) return -1
+        this.moveToHead(node)
+        return node.value
+    }
+
+    put(key, value) {
+        let node = this.hashTable[key]
+        if (node == null) {
+            let newNode = new ListNode(key, value)
+            this.hashTable[key] = newNode
+            this.addToHead(newNode)
+            this.count++
+            if (this.count > this.capacity) {
+                this.removeLRUItem()
+            }
+        } else {
+            node.value = value
+            this.moveToHead(node)
+        }
+    }
+
+    moveToHead(node) {
+        this.removeFromList(node)
+        this.addToHead(node)
+    }
+
+    removeFromList(node) {
+        // 想实现的是 node.prev -> node.next 的双向
+        const tempForPrev = node.prev
+        const tempForNext = node.next
+        node.prev.next = node.next
+        tempForNext.prev = tempForPrev
+    }
+
+    addToHead(node) {
+        node.prev = this.dummyHead
+        node.next = this.dummyHead.next
+        this.dummyHead.next.prev = node
+        this.dummyHead.next = node
+    }
+
+    removeLRUItem() {
+        let tail = this.popTail()
+        delete this.hashTable[tail.key]
+        this.count--
+    }
+
+    popTail() {
+        let tailItem = this.dummyTail.prev
+        this.removeFromList(tailItem)
+        return tailItem
+    }
+}
 // @lc code=end
 
 var cache = new LRUCache(2);
